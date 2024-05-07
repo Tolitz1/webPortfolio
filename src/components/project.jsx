514c77c3743665eeed28e2d6a7f0634db66bcb4d
Tolitz1 <116473735+Tolitz1@users.noreply.github.com>
2024-05-07 16:15:17 +0800

    icon update

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -19,8 +19,8 @@ export default function Project() {
                                     Appointment System
                                 </div>
                                 <a href="#" className="font-semibold">Southern Luzon State University</a>
-                                <p className="mt-2 text-gray-500">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ipsam saepe mollitia, vel obcaecati quia iusto doloribus facilis modi, quam impedit voluptate illo tempore cupiditate tenetur expedita accusantium et! Pariatur?
+                                <p className="mt-2 text-gray-500 text-justify">
+                                At SLSU, we've developed an appointment system using Laravel, a powerful PHP framework. This system is designed to streamline the process of scheduling appointments, making it easier for both employees and students to manage their time effectively.
                                 </p>
                             </div>
                             <div className="md:shrink-0 p-5">
@@ -35,8 +35,8 @@ export default function Project() {
                                     Document Tracking System
                                 </div>
                                 <a href="#" className="font-semibold">Southern Luzon State University</a>
-                                <p className="mt-2 text-gray-500">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ipsam saepe mollitia, vel obcaecati quia iusto doloribus facilis modi, quam impedit voluptate illo tempore cupiditate tenetur expedita accusantium et! Pariatur?
+                                <p className="mt-2 text-gray-500 text-justify">
+                                This system is designed to efficiently track the movement and status of documents within the university, ensuring smooth workflow and accountability. Document Tracking System was built using CodeIgniter 3.
                                 </p>
                             </div>
                             <div className="md:shrink-0 p-5">
@@ -54,8 +54,8 @@ export default function Project() {
                                     Supply Inventory System
                                 </div>
                                 <a href="#" className="font-semibold">Southern Luzon State University</a>
-                                <p className="mt-2 text-gray-500">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ipsam saepe mollitia, vel obcaecati quia iusto doloribus facilis modi, quam impedit voluptate illo tempore cupiditate tenetur expedita accusantium et! Pariatur?
+                                <p className="mt-2 text-gray-500 text-justify">
+                                In collaboration with my former colleague from SLSU, we've developed a Supply Inventory System using CodeIgniter 3, a reliable PHP framework. This system aims to efficiently manage the university's supply inventory, ensuring that the right items are available when needed and minimizing wastage or shortages.
                                 </p>
                             </div>
                             <div className="md:shrink-0 p-5">
@@ -70,8 +70,8 @@ export default function Project() {
                                    MIS Borrowing System
                                 </div>
                                 <a href="#" className="font-semibold">Southern Luzon State University</a>
-                                <p className="mt-2 text-gray-500">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ipsam saepe mollitia, vel obcaecati quia iusto doloribus facilis modi, quam impedit voluptate illo tempore cupiditate tenetur expedita accusantium et! Pariatur?
+                                <p className="mt-2 text-gray-500 text-justify">
+                                This system facilitates the management of borrowing processes within the university, ensuring smooth transactions and accountability. MIS Borrowing System was built using CodeIgniter 3.
                                 </p>
                             </div>
                             <div className="md:shrink-0 p-5">
@@ -89,8 +89,8 @@ export default function Project() {
                                 Digitization of Documents
                                 </div>
                                 <a href="#" className="font-semibold">Southern Luzon State University</a>
-                                <p className="mt-2 text-gray-500">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ipsam saepe mollitia, vel obcaecati quia iusto doloribus facilis modi, quam impedit voluptate illo tempore cupiditate tenetur expedita accusantium et! Pariatur?
+                                <p className="mt-2 text-gray-500 text-justify">
+                                At SLSU's Registrar Department, I created a Digitization of Documents System using CodeIgniter 3. This system aims to transition from traditional paper-based document management to a digital platform, facilitating efficient document storage, retrieval, and management.
                                 </p>
                             </div>
                             <div className="md:shrink-0 p-5">
