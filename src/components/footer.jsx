d40f1ad86ac52614f61d17bb97d4d2ac0fa5f3dd
Tolitz1 <116473735+Tolitz1@users.noreply.github.com>
2024-05-08 09:37:48 +0800

    update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,16 +5,21 @@ import { MdCopyright } from "react-icons/md";
 export default function Footer() {
   return (
 <div className="flex flex-col bg-gray-800 text-white py-5 px-5 justify-center items-center">
-  <div className="flex items-center mb-4">
-    <span className="font-semibold mr-4 flex items-center"><CiLocationArrow1 className="mr-2" />Lucban, Quezon Philippines</span>
-    <span className="mr-4 flex items-center"><FiPhone className="mr-2" />+63 915 160 6219</span>
-    <span className="flex items-center"><CiMail className="mr-2" />alvinsalvacion06@gmail.com</span>
+  <div className="flex flex-wrap justify-center items-center mb-4">
+    <div className="mb-4 sm:mb-0 sm:mr-4 flex items-center">
+      <span className="font-semibold mr-4 flex items-center"><CiLocationArrow1 className="mr-2" />Lucban, Quezon Philippines</span>
+    </div>
+    <div className="mb-4 sm:mb-0 sm:mr-4 flex items-center">
+      <span className="mr-4 flex items-center"><FiPhone className="mr-2" />+63 915 160 6219</span>
+    </div>
+    <div className="mb-4 sm:mb-0 sm:mr-4 flex items-center">
+      <span className="flex items-center"><CiMail className="mr-2" />alvinsalvacion06@gmail.com</span>
+    </div>
   </div>
   <hr />
  <div className="flex items-center">
     <span className="flex items-center"><MdCopyright className="mr-2" />2024 All Rights Reserved</span>
   </div>
 </div>
-
   )
 }
