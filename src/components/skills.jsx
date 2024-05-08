2dc590eea57beea0da6672018652dc153710b6f7
Tolitz1 <116473735+Tolitz1@users.noreply.github.com>
2024-05-08 11:35:39 +0800

    skills.jsx

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,15 +1,59 @@
-
-export default function Skills() {
+import {Swiper, SwiperSlide} from "swiper/react";
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/free-mode'
+import { FreeMode, Pagination } from "swiper/modules";
+import { RxArrowTopRight } from "react-icons/rx";
+import { ServiceData } from "../constants";
+const skills = () => {
   return (
     <div>
-      <div className="relative rounded-xl overflow-auto p-8 hover:animate-pulse">
-        <div className="overflow-hidden relative max-w-md mx-auto bg-white shadow-lg ring-1 ring-black/5 rounded-xl flex item-center gap-6 dark:bg-slate-800 dark:highlight-white/5">
-
-        </div>
+     
+      <div className="flex item-center justify-center flex-col h-screen">
+        <Swiper
+          breakpoints={{
+            375: {
+              slidesPerView: 1,
+              spaceBetween: 15,
+            },
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 15,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 15,
+            },
+          }}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="max-w-[90%] lg:max-w-[80%]"
+        >
+          {ServiceData.map((item) => (
+            <SwiperSlide key={item.title}>
+              <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] ssm:h-[500px] ssm:w-[300px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer">
+                <div
+                  className="absolute inset-0 bg-cover bg-center bg-gray-600"
+                />
+                <div className="absolute inset-0 bg-gray-800 opacity-10 group-hover:opacity-50" />
+                <a href={item.href} target="_blank">
+                <div className="relative flex flex-col gap-3">
+                  <item.icon className="text-blue-600 group-hover:text-blue-400 w-[32px] h-[32px]" />
+                  <h1 className="text-xl lg:text-2xl">{item.title} </h1>
+                  <p className="lg:text-[16px]">{item.content} </p>
+                </div>
+                <RxArrowTopRight className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-blue-500 group-hover:rotate-45 duration-100" />
+              </a>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-      <div></div>
-      <div></div>
-      <div></div>
     </div>
   )
 }
+
+export default skills
---
+++ src/constants/index.jsx
@@ -0,0 +1,65 @@
+
+  import { SiCodeigniter, SiLaravel, SiReact, SiBootstrap, SiTailwindcss, SiJavascript, SiMysql } from "react-icons/si";
+  import { FaHtml5, FaCss3, FaPhp } from "react-icons/fa";
+  export const ServiceData = [
+    {
+      icon: SiCodeigniter,
+      title: "CodeIgniter",
+      content: "CodeIgniter is a powerful PHP framework with a very small footprint, built for developers who need a simple and elegant toolkit to create full-featured web applications.",
+      href: "https://codeigniter.com/"
+    },
+    {
+      icon: SiLaravel,
+      title: "Laravel",
+      content: "Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.",
+      href: "https://laravel.com/"
+    },
+    {
+      icon: SiReact,
+      title: "React",
+      content: "React lets you build user interfaces out of individual pieces called components. Create your own React components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps.",
+      href: "https://react.dev/"
+    },
+    {
+      icon: SiTailwindcss,
+      title: "Tailwind",
+      content: "A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.",
+      href: "https://tailwindcss.com/"
+    },
+    {
+      icon: SiBootstrap,
+      title: "Bootstrap",
+      content: "Powerful, extensible, and feature-packed frontend toolkit. Build and customize with Sass, utilize prebuilt grid system and components, and bring projects to life with powerful JavaScript plugins.",
+      href: "https://getbootstrap.com/"
+    },
+    {
+        icon: FaPhp,
+        title: "PHP",
+        content: "PHP (recursive acronym for PHP: Hypertext Preprocessor) is a widely-used open source general-purpose scripting language that is especially suited for web development and can be embedded into HTML.",
+        href: "https://www.php.net/manual/en/intro-whatis.php"
+      },
+    {
+      icon: SiJavascript,
+      title: "Javascript",
+      content: "JavaScript is the Programming Language for the Web. JavaScript can update and change both HTML and CSS. JavaScript can calculate, manipulate and validate data.",
+      href: "https://www.w3schools.com/whatis/whatis_js.asp"
+    },
+    {
+       icon: FaHtml5,
+       title: "HTML5",
+       content: "HTML5 was designed to do virtually anything you want to do online without having to download browser plugins or other software. Want to create animations? Embed music and movies? Build advanced applications that run in your browser? You can with HTML5.",
+       href: "https://blog.hubspot.com/blog/tabid/6307/bid/5847/a-marketer-s-guide-to-html5.aspx"
+    },
+    {
+       icon: FaCss3,
+       title: "CSS",
+       content: "CSS is one of the three core Web technologies, along with HTML and JavaScript. CSS usually styles HTML elements, but can be also used with other markup languages like SVG or XML.",
+       href: "https://developer.mozilla.org/en-US/docs/Glossary/CSS"
+    },
+    {
+        icon: SiMysql,
+        title: "Mysql",
+        content: "The SQL part of “MySQL” stands for “Structured Query Language”. SQL is the most common standardized language used to access databases. ",
+        href: "https://dev.mysql.com/doc/refman/8.0/en/what-is-mysql.html"
+     },
+  ];
